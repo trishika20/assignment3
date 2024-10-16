8dbd7727fbfdfc82f690f5bec06d869e339ca44f
Trishika Sakhuja <c0911589@mylambton.ca>
2024-10-16 19:37:50 -0400

    fix cicd

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,30 @@
+
 provider "azurerm" {
   features {}
+
+  # You can either hardcode your subscription_id here or rely on Azure CLI authentication
+  # Uncomment the next line if you'd like to hardcode the subscription ID
+  
 }
 
+# Resource Group definition
 resource "azurerm_resource_group" "my_rg" {
   name     = "myResourceGroup"
   location = "East US"
 }
 
+# App Service Plan definition
 resource "azurerm_app_service_plan" "my_plan" {
   name                = "myAppServicePlan"
   location            = azurerm_resource_group.my_rg.location
   resource_group_name = azurerm_resource_group.my_rg.name
   sku {
-    tier = "Basic"
-    size = "B1"
+    tier = "Free"
+    size = "F1"
   }
 }
 
+# App Service definition
 resource "azurerm_app_service" "my_app" {
   name                = "myAppService"
   location            = azurerm_resource_group.my_rg.location
@@ -24,12 +32,13 @@ resource "azurerm_app_service" "my_app" {
   app_service_plan_id = azurerm_app_service_plan.my_plan.id
 
   site_config {
-    always_on = true
+    always_on        = true
+    linux_fx_version = "NODE|14-lts"  # For Node.js applications
   }
 
   app_settings = {
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
-    WEBSITES_PORT                       = "3000"
+    WEBSITES_PORT                       = "3000"  # Ensure this port matches the port used by your app
   }
 
   lifecycle {
@@ -37,5 +46,11 @@ resource "azurerm_app_service" "my_app" {
       app_settings["WEBSITES_PORT"],
     ]
   }
+}
+
+# Output the default hostname of the app service
+output "app_service_default_hostname" {
+  value       = azurerm_app_service.my_app.default_site_hostname
+  description = "The default hostname for the Azure App Service"
 }
 
